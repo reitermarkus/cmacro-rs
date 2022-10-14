ce7073e2610d6219cd44c63f81c700e3a9ae3e12
Markus Reiter <me@reitermark.us>
2022-10-14 20:10:50 +0200

    Fix formatting.

--- tests/fixtures/precedence.rs
+++ tests/fixtures/precedence.rs
@@ -1,7 +1,7 @@
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __cmacro__prec_rem_div1 {
-  ($ a : expr , $ b : expr , $ c : expr) => {
+  ($a:expr, $b:expr, $c:expr) => {
     $a % $b / $c
   };
 }
@@ -10,7 +10,7 @@ pub use __cmacro__prec_rem_div1 as prec_rem_div1;
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __cmacro__prec_rem_div2 {
-  ($ a : expr , $ b : expr , $ c : expr) => {
+  ($a:expr, $b:expr, $c:expr) => {
     $a % $b / $c
   };
 }
@@ -19,7 +19,7 @@ pub use __cmacro__prec_rem_div2 as prec_rem_div2;
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __cmacro__prec_rem_div3 {
-  ($ a : expr , $ b : expr , $ c : expr) => {
+  ($a:expr, $b:expr, $c:expr) => {
     $a % ($b / $c)
   };
 }
