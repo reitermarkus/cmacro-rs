41432efd8b004789121cb80173c40b7f5cdaa60d
Markus Reiter <me@reitermark.us>
2023-03-17 05:56:30 +0100

    Implement `Display` for `Error`.

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 /// A parsing or codegen error.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Error {
@@ -9,4 +11,15 @@ pub enum Error {
   UnsupportedExpression,
   /// Recursive macro definition.
   RecursiveDefinition(String),
+}
+
+impl fmt::Display for Error {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      Self::UnknownVariable(var_name) => write!(f, "unknown variable {}", var_name),
+      Self::ParserError => write!(f, "parser error"),
+      Self::UnsupportedExpression => write!(f, "unsupported expression"),
+      Self::RecursiveDefinition(macro_name) => write!(f, "recursive macro definition {}", macro_name),
+    }
+  }
 }
